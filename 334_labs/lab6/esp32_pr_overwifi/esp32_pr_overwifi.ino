018e70f714c422d321a779f2db14ac3105099f8b
maliakuo <70962881+maliakuo@users.noreply.github.com>
2023-10-12 17:18:59 -0400

    lab 6

---
+++ 334_labs/lab6/esp32_pr_overwifi/esp32_pr_overwifi.ino
@@ -0,0 +1,53 @@
+#include <WiFi.h>
+#include <WiFiUdp.h>
+
+
+const int pin = 35; // Change this to the desired analog pin
+
+
+const char* ssid = "yale wireless";
+
+
+const char* udpServerIP = "172.29.134.110"; // Change to the target IP address
+const int udpServerPort = 12345; // Change to the target port number
+
+
+WiFiUDP udp;
+
+
+void setup() {
+pinMode(pin, INPUT); // Set the pin as an analog input
+Serial.begin(115200); // Initialize serial communication
+
+
+// Connect to Wi-Fi
+WiFi.begin(ssid);
+while (WiFi.status() != WL_CONNECTED) {
+Serial.println("Connecting to WiFi...");
+}
+Serial.println("Connected to WiFi");
+
+
+// Begin the UDP connection
+udp.begin(udpServerPort);
+}
+
+
+void loop() {
+int sensorValue = analogRead(pin); // Read the analog input
+Serial.println(sensorValue);
+// Data to be sent
+String message = "Hello, UDP Server!";
+
+
+// Send the UDP packet
+udp.beginPacket(udpServerIP, udpServerPort);
+udp.print(sensorValue);
+udp.endPacket();
+
+
+Serial.print("Allegedly sent: ");
+Serial.println(sensorValue);
+
+
+}
---
+++ 334_labs/lab6/visualize_photresistor.py
@@ -0,0 +1,29 @@
+import socket
+import os
+import math
+
+
+ip_address = "0.0.0.0"
+port = 12345
+
+
+udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+
+udp_socket.bind((ip_address, port))
+
+
+print(f"Listening for UDP packets on {ip_address}:{port}...")
+
+
+def visualize(size):
+    res = []
+    for i in range(size // 40):
+        res.append('o')
+        print(''.join(res))
+
+
+while True:
+    data, address = udp_socket.recvfrom(1024)
+    size = int(data.decode('utf-8'))
+    visualize(size)
